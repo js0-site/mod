618c6c97218645f01ce7d81c2bb43e7cdd4c260d
js0.site <jssite@googlegroups.com>
2025-12-11 16:18:39 +0800

    init

---
+++ srv/gen/js/js0_srv/Api.js
@@ -0,0 +1,5 @@
+import $ from "js0.site/~.js"
+import google$protobuf$EmptyE from "../google/protobuf/EmptyE.js"
+import js0_srv$CaptchaD from "./CaptchaD.js"
+export const _=undefined,
+captchaNew = $(1,google$protobuf$EmptyE,js0_srv$CaptchaD)
